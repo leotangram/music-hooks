543184f9b862c59f320e58f80e59a3ce1a3a4ce1
Leonardo Omaña <leotangram@gmail.com>
2019-06-15 21:38:23 -0500

    useEffect second consult.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, Fragment } from 'react'
 import Form from './components/Form'
 import axios from 'axios'
-import Song from './components/Song';
+import Song from './components/Song'
 
 function App() {
   // Utilizar useSsate con 3 states diferentes
@@ -18,9 +18,25 @@ function App() {
     // Consultar la API
     const result = await axios(url)
 
+    // Almacenar el artista que se buscó
+    setArtist(artist)
+
     // Almacenar la letra en el state
     setLyric(result.data.lyrics)
   }
+
+  // Método para consultar la API de información de la banda
+  const consultAPIInfo = async () => {
+    const url = `https://theaudiodb.com/api/v1/json/1/search.php?s=${artist}`
+
+    const result = await axios(url)
+
+    setInfo(result.data.artists[0])
+  }
+
+  useEffect(() => {
+    consultAPIInfo()
+  }, [artist])
 
   return (
     <Fragment>
