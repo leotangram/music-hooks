513907e74da20a2d9e00e4c3e4775b7b0765371e
Leonardo Omaña <leotangram@gmail.com>
2019-06-15 21:15:18 -0500

    Consult lyric api.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, Fragment } from 'react'
 import Form from './components/Form'
+import axios from 'axios'
 
 function App() {
   // Utilizar useSsate con 3 states diferentes
@@ -8,8 +9,16 @@ function App() {
   const [info, setInfo] = useState({})
 
   // Método para consultar la API de letras de canciones
-  const consultAPILetter = search => {
+  const consultAPILetter = async search => {
     console.log(search)
+    const { artist, song } = search
+    const url = `https://api.lyrics.ovh/v1/${artist}/${song}`
+
+    // Consultar la API
+    const result = await axios(url)
+
+    // Almacenar la letra en el state
+    setLyric(result.data.lyrics)
   }
 
   return (
